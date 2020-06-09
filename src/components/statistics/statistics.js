2f0c05c6c19210d6252017dc250dea6b53fabfcb
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-06-09 12:20:41 +0300

    upgrade

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -2,46 +2,45 @@ import React from "react";
 import "./statistics.css";
 
 function Statistics(props) {
-  const { all, done, imp, onClickElement } = props;
-  const barStyle = { paddingLeft: "" };
+    const { all, done, imp, onClickElement } = props;
+    let barStyle = {paddingLeft: ""};
 
-  barStyle.paddingLeft = (Number(done) * 100) / Number(all) + "%";
+    barStyle.paddingLeft = (Number(done) * 100) / Number(all) + "%";
 
-  return (
-    <section>
-      <p className="text-right">
-        <span
-          className="badge badge-success"
-          onClick={() => onClickElement("ONLYDONE")}
-        >
-          DONE: {done}
-        </span>
-        <span
-          className="badge ml-1 badge-primary"
-          onClick={() => onClickElement("ONLYIMPORTANT")}
-        >
-          IMP: {imp}
-        </span>
-        <span
-          className="badge badge-dark ml-1"
-          onClick={() => onClickElement("ALLTASKS")}
-        >
-          ALL: {all}
-        </span>
-      </p>
+    return (
+        <section>
+            <p className="text-right">
+                <span
+                    className="badge badge-success"
+                    onClick={ () => onClickElement("ONLYDONE") }>
 
-      <div className="progress mb-3">
-        <div
-          className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-          role="progressbar"
-          aria-valuenow={done}
-          aria-valuemin="0"
-          aria-valuemax={all}
-          style={barStyle}
-        />
-      </div>
-    </section>
-  );
+                    DONE: { done }
+                </span>
+                <span
+                    className="badge ml-1 badge-primary"
+                    onClick={ () => onClickElement("ONLYIMPORTANT") }>
+                    
+                    IMP: { imp }
+                </span>
+                <span
+                    className="badge badge-dark ml-1"
+                    onClick={ () => onClickElement("ALLTASKS") }>
+                    
+                    ALL: { all }
+                </span>
+            </p>
+
+            <div className="progress mb-3">
+                <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    aria-valuenow={ done }
+                    aria-valuemin="0"
+                    aria-valuemax={ all }
+                    style={ barStyle } />
+            </div>
+        </section>
+    );
 }
 
 export default Statistics;
--- src/components/task-form/task-form.js
+++ src/components/task-form/task-form.js
@@ -3,32 +3,32 @@ import React from "react";
 import "./task-form.css";
 
 function TaskForm(props) {
-  const { onClickElement } = props;
-  let value = "New Task";
+    const { onClickElement } = props;
+    let value = "New Task";
 
-  function inputChange(e) {
-    value = e.target.value;
-  }
+    function inputChange(e) {
+        value = e.target.value;
+    }
 
-  return (
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Задача"
-        onChange={inputChange}
-      />
-      <div className="input-group-append">
-        <button
-          className="btn btn-outline-secondary bg-info text-white"
-          type="button"
-          onClick={() => onClickElement("ADD", null, value)}
-        >
-          Добавить
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="input-group mb-3">
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Задача"
+                onChange={ inputChange } />
+          
+            <div className="input-group-append">
+                <button
+                    className="btn btn-outline-secondary bg-info text-white"
+                    type="button"
+                    onClick={ () => onClickElement("ADD", null, value) }>
+                    
+                    Добавить
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default TaskForm;
